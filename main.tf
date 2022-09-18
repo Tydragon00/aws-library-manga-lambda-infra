ea86d590d8c82595e9147fae22f9f615e3e589a0
Tydragon00 <timothydrago@gmail.com>
2022-09-18 14:04:08 +0200

    update

--- main.tf
+++ main.tf
@@ -63,6 +63,13 @@ resource "aws_iam_role_policy_attachment" "basic_execution" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+#Lambda Cloudwatch
+resource "aws_cloudwatch_log_group" "library-manga-lambda-cloudwatch" {
+  name = "/aws/lambda/library-manga-lambda-test"
+
+
+}
+
 
 resource "aws_lambda_function" "library-manga-lambda" {
   s3_bucket     = "library-manga-bucket"
